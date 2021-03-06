6484fd3d1a761249f92845e9f7ab5facbb19f759
cdroot92 <cdroot92@gmail.com>
2021-03-06 13:12:24 +0900

    Add thread for async

--- todos/signals.py
+++ todos/signals.py
@@ -1,3 +1,6 @@
+import time
+from threading import Thread
+
 from django.core.signals import request_finished
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -8,3 +11,12 @@ from . import models
 @receiver(post_save, sender=models.TodoList)
 def on_post_save_sender(sender, **kwargs):
     print(f"{sender} post_save")
+    t = Thread(target=long_task)
+    t.start()
+    print(f"{sender} post_save_end")
+
+
+def long_task():
+    print(f"start long task")
+    time.sleep(10)
+    print(f"end long task")
